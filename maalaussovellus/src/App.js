010d609b30f5dada7623a38d1cab1c9a603aa672
Jenni Luotonen <jenni.luotonen1@edu.turkuamk.fi>
2020-12-12 20:41:58 +0200

    lomakkeen alkua

--- maalaussovellus/src/App.js
+++ maalaussovellus/src/App.js
@@ -1,19 +1,53 @@
-import logo from './logo.svg';
+import React, { useReducer, useState } from "react";
+import ReactDOM from "react-dom";
 import './App.css';
 
-export default function App() {
-  const [maalinNimi, setMaalinNimi] = useState('');
 
-  return (
-    <div id="container">
-    <View>
-      <Text>Kirjoita maalin nimi:</Text>
-      <TextInput
-        onChangeText={(val) => setMaalinNimi(val)} />
-    </View>
-    </div>
-  );
-}
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+  }
+  
+  function App() {
+    const [formData, setFormData] = useReducer(formReducer, {});
+  
+    const handleChange = event => {
+      setFormData({
+        name: event.target.name,
+        value: event.target.value,
+      });
+    }
+  
+    return(
+      <div className="wrapper">
+        <h1>Maalilaskuri</h1>
+  
+        <form onSubmit={handleSubmit}>
+            <label>
+              <p>Maalin nimi</p>
+              <input name="nimi" onChange={handleChange} value={formData.nimi}/>
+            </label>
+            <label>
+            <p>Maalin hinta (€/litra)</p>
+              <input name="hinta" onChange={handleChange} value={formData.hinta}/>
+            </label>
+            <label>
+            <p>Maalattava pinta-ala (m2)</p>
+              <input name="maara" onChange={handleChange} value={formData.maara}/>
+            </label>
+            <label>
+            <p>Maakauskertoja</p>
+              <input name="kerroin" onChange={handleChange} value={formData.kerroin}/>
+            </label>
+          <button type="submit">Lasketaan maalauksen hinta</button>
+        </form>
+      </div>
+    )
+  }
+   
+   ReactDOM.render(<App />, document.getElementById("root"));
 
 
 
