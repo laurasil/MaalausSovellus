3bd5e6ba0aeb1c997e0fd23d21d30060c664afa4
Laura Siltanen <laura.siltanen@edu.turkuamk.fi>
2020-12-14 14:11:40 +0200

    huone ja asunto muutoksia

--- maalaussovellus/src/components/asunto.js
+++ maalaussovellus/src/components/asunto.js
@@ -2,14 +2,14 @@ import React from "react";
 import Huone from "./huone";
 
 // tarvitaan vielä maalin tiedot käyttäjältä
-const Asunto = ({asunto, nimi, huoneet, tyyppikerroinKA, kokonaisPintaAla, maaliNimi, hinta, riittoisuus }) => {
+const Asunto = ({asunto, tyyppikerroinKA, kokonaisPintaAla, maaliNimi, hinta, riittoisuus, maara }) => {
 
-    var asunnonMaalinTarve = (kokonaisPintaAla / riittoisuus) * tyyppikerroinKA;
+    var asunnonMaalinTarve = ((kokonaisPintaAla / riittoisuus) * tyyppikerroinKA) * maara;
 
     return (
         <div>
             <h2><u>{asunto}:</u></h2>
-            <p>Asunnon maalaukseen maalia kuluu {asunnonMaalinTarve} litraa ja kustannukset ovat {hinta*asunnonMaalinTarve}.</p>
+            <p>Asunnon maalaukseen maalia {maaliNimi} kuluu {Math.round(asunnonMaalinTarve * 100) / 100} litraa ja kustannukset ovat {Math.round((hinta*asunnonMaalinTarve) * 100) / 100}€.</p>
         </div>
     )
 }
--- maalaussovellus/src/components/huone.js
+++ maalaussovellus/src/components/huone.js
@@ -3,14 +3,14 @@ import React from "react";
 // tarvitaan vielä maalin tiedot käyttäjältä
 const Huone = ({ nimi, pintaAla, tyyppikerroin, maaliNimi, hinta, maara, riittoisuus }) => {
 
-    var maalinTarve = (pintaAla / riittoisuus) * tyyppikerroin;
+    var maalinTarve = ((pintaAla / riittoisuus) * tyyppikerroin) * maara;
     var maalinKustannus = maalinTarve * hinta;
 
     return (
         <div>
             <h2><u>{nimi}:</u></h2>
             {/*pyöristetään hinta kahteen desimaaliin {Math.round()/100} avulla*/}
-            <p>Huoneeseen tarvitaan maalia {maalinTarve} ja hinta on {Math.round(maalinKustannus * 100) / 100}€.</p>
+            <p>Huoneeseen tarvitaan maalia {maaliNimi} {Math.round(maalinTarve * 100) / 100} litraa ja hinta on {Math.round(maalinKustannus * 100) / 100}€.</p>
         </div>
     );
 }
